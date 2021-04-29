65f5502ca36e512c0e7ef615d446bdfa6ea78cab
himoto <31299606+himoto@users.noreply.github.com>
2021-04-29 10:42:38 +0900

    Update dyaus-dev

--- dyaus_dev/patient_model.py
+++ dyaus_dev/patient_model.py
@@ -1,10 +1,16 @@
+import csv
 import multiprocessing
+import os
 import platform
 from dataclasses import dataclass, field
 from importlib import import_module
-from typing import Any, Callable, List, Optional
+from typing import Any, Callable, Dict, List, Optional
 
+import numpy as np
+import pandas as pd
+import seaborn as sns
 from biomass import ModelObject, run_analysis, run_simulation
+from scipy.integrate import simps
 from tqdm import tqdm
 
 
@@ -131,6 +137,141 @@ class PatientModelSimulations(InSilico):
 
         self.parallel_execute(self._run_single_patient, n_proc)
 
+    @staticmethod
+    def _calc_response_characteristics(
+        time_course_data: np.ndarray,
+        metric: str,
+    ) -> str:
+        if metric.lower() == "max":
+            response_characteristics = np.max(time_course_data)
+        elif metric.lower() == "auc":
+            response_characteristics = simps(time_course_data)
+        elif metric.lower == "droprate":
+            response_characteristics = (np.max(time_course_data) - time_course_data[-1]) / (
+                len(time_course_data) - np.argmax(time_course_data)
+            )
+        else:
+            raise ValueError("Available metrics are: 'max', 'AUC', 'droprate'.")
+
+        return str(response_characteristics)
+
+    def _extract(
+        self,
+        dynamic_characteristics: Dict[str, Dict[str, List[str]]],
+        normalization: bool,
+    ) -> None:
+        """
+        Extract response characteristics from patient-specific signaling dynamics.
+        """
+        os.makedirs("classification", exist_ok=True)
+        for obs_name, conditions_and_metrics in dynamic_characteristics.items():
+            with open(
+                os.path.join("classification", f"{obs_name}.csv"),
+                "w",
+                newline="",
+            ) as f:
+                writer = csv.writer(f)
+                header = ["Sample"]
+                for condition, metrics in conditions_and_metrics.items():
+                    for metric in metrics:
+                        header.append(f"{condition}_{metric}")
+                writer.writerow(header)
+
+                for patient in self.patients:
+                    biomass_model = self.import_model_package(patient.strip())
+                    patient_specific = biomass_model.create()
+                    all_data = np.load(
+                        os.path.join(
+                            patient_specific.path,
+                            "simulation_data",
+                            "simulations_all.npy",
+                        )
+                    )
+                    data = np.array(all_data[patient_specific.obs.index(obs_name)])
+                    if normalization:
+                        for i in range(data.shape[0]):
+                            if not np.isnan(data[i]).all():
+                                data[i] /= np.nanmax(data[i])
+                        data = np.nanmean(data, axis=0)
+                        data /= np.max(data)
+                    patient_specific_characteristics = [patient]
+                    for h in header[1:]:
+                        condition, metric = h.split("_")
+                        patient_specific_characteristics.append(
+                            self._calc_response_characteristics(
+                                data[:, patient_specific.sim.conditions.index(condition)],
+                                metric,
+                            )
+                        )
+                    writer = csv.writer(f, lineterminator="\n")
+                    writer.writerow(patient_specific_characteristics)
+
+    def subtyping(
+        self,
+        fname: Optional[str],
+        dynamic_characteristics: Dict[str, Dict[str, List[str]]],
+        normalization: bool = True,
+        *,
+        clustermap_kws: Optional[dict] = None,
+    ):
+        """
+        Classify patients based on dynamic characteristics extracted from simulation results.
+
+        Parameters
+        ----------
+        fname : str, path-like or None
+            The clustermap is saved as fname if it is not None.
+
+        dynamic_characteristics : Dict[str, Dict[str, List[str]]]
+            {"observable": {"condition": ["metric", ...], ...}, ...}.
+            Characteristics in the signaling dynamics used for classification.
+            'metric' must be one of 'max', 'AUC', 'droprate'.
+
+        normalization : bool (default: True)
+            Whether to perform max-normalization.
+
+        clustermap_kws : dict, optional (default: None)
+            Keyword arguments to pass to seaborn.clustermap().
+
+        Examples
+        --------
+        >>> with open ("models/breast/sample_names.txt", mode="r") as f:
+                TCGA_ID = f.read().splitlines()
+        >>> from dyaus import PatientModelSimulations
+        >>> simulations = PatientModelSimulations("models.breast", TCGA_ID)
+        >>> simulations.subtyping(
+                "subtype_classification.pdf",
+                {
+                    "Phosphorylated_Akt": {"EGF": ["max"], "HRG": ["max"]},
+                    "Phosphorylated_ERK": {"EGF": ["max"], "HRG": ["max"]},
+                    "Phosphorylated_c-Myc": {"EGF": ["max"], "HRG": ["max"]},
+                },
+                clustermap_kws={"figsize": (9, 12)}
+            )
+        """
+        # seaborn clustermap
+        if clustermap_kws is None:
+            clustermap_kws = {}
+        clustermap_kws.setdefault("z_score", 1)
+        clustermap_kws.setdefault("cmap", "RdBu_r")
+        clustermap_kws.setdefault("center", 0)
+        # extract response characteristics
+        self._extract(dynamic_characteristics, normalization)
+        if fname is not None:
+            characteristics: List[pd.DataFrame] = []
+            files = os.listdir("classification")
+            for file in files:
+                obs, ext = os.path.splitext(file)
+                if ext == ".csv":
+                    df = pd.read_csv(os.path.join("classification", file), index_col="Sample")
+                    characteristics.append(
+                        df.rename(columns=lambda s: obs.replace("_", " ") + "_" + s)
+                    )
+            all_info = pd.concat(characteristics, axis=1)
+            all_info.index.name = ""
+            fig = sns.clustermap(all_info, **clustermap_kws)
+            fig.savefig(fname)
+
 
 @dataclass
 class PatientModelAnalyses(InSilico):
@@ -160,7 +301,7 @@ class PatientModelAnalyses(InSilico):
             kwargs = {}
         kwargs.setdefault("target", "initial_condition")
         kwargs.setdefault("metric", "integral")
-        kwargs.setdefault("style", "barplot")
+        kwargs.setdefault("style", "heatmap")
         kwargs.setdefault("options", None)
 
         biomass_model = self.import_model_package(patient.strip())
